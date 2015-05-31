4f7fb5f5ef8df06b424ff71fed54164a3c6c48ac
Yuusuke Matsuura <matsuura.kou@gmail.com>
2015-05-31 21:38:02 +0900

    DB操作用のクラス追加

---
+++ src/main/java/app/sample/model/Tasks.java
@@ -0,0 +1,35 @@
+package app.sample.model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.sample.bean.TasksBean;
+
+public class Tasks{
+
+    private String selectAll = "SELECT * FROM tasks";
+
+    public List printSelectAll() throws SQLException {
+        Connection con = DatabaseManager.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet result = stmt.executeQuery(selectAll);
+        ArrayList<TasksBean> taskList = new ArrayList();
+        while (result.next()) {
+            TasksBean tasks = new TasksBean();
+            tasks.setId(result.getInt(1));
+            tasks.setTitle(result.getString(2));
+            tasks.setFinished(result.getInt(3));
+            tasks.setVersion(result.getInt(4));
+            tasks.setCreatedAt(result.getTimestamp(5));
+            tasks.setUpdatedAt(result.getTimestamp(6));
+            taskList.add(tasks);
+        }
+        System.out.print(taskList);
+        return taskList;
+    }
+
+}
---
+++ src/test/java/app/sample/model/TasksTest.java
@@ -0,0 +1,54 @@
+package app.sample.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import app.sample.bean.TasksBean;
+import app.sample.model.Tasks;
+
+
+public class TasksTest {
+
+    // テストクラスのstaticイニシャライザの後に呼ばれる。
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        //System.out.println("@BeforeClass call");
+    }
+
+    // テストクラス実行後に実行したいメソッドに付与
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        //System.out.println("@AfterClass call");
+    }
+
+    // テストクラスのコンストラクタの後に呼ばれる。
+    @Before
+    public void setUp() throws Exception {
+        //System.out.println("@Before call");
+    }
+
+    // テストメソッド実行後に実行したいメソッドに付与
+    @After
+    public void tearDown() throws Exception {
+        //System.out.println("@After call");
+    }
+
+    // テストメソッドに付与
+    @Test
+    public void printSelectAllTest() throws SQLException {
+        Tasks task = new Tasks();
+        List taskList = task.printSelectAll();
+        assertEquals(taskList.size(),5);
+    }
+
+
+
+}
