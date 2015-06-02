bffe21426f9f07accdbfeea9e14a63a466069884
Yuusuke Matsuura <matsuura.kou@gmail.com>
2015-06-02 13:49:49 +0900

    Insert文を追加

--- src/main/java/app/sample/model/Tasks.java
+++ src/main/java/app/sample/model/Tasks.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +12,11 @@ import app.sample.bean.TasksBean;
 
 public class Tasks{
 
-
     public List<TasksBean> selectAll() throws SQLException {
-        String selectAll = "SELECT * FROM tasks";
+        String sql = "SELECT * FROM tasks";
         Connection con = DatabaseManager.getConnection();
         Statement stmt = con.createStatement();
-        ResultSet result = stmt.executeQuery(selectAll);
+        ResultSet result = stmt.executeQuery(sql);
         ArrayList<TasksBean> taskList = new ArrayList<TasksBean>();
         while (result.next()) {
             TasksBean tasks = new TasksBean();
@@ -33,10 +33,12 @@ public class Tasks{
     }
 
     public TasksBean selectById(int id) throws SQLException{
-        String selectOne = "SELECT * FROM tasks where id =" + id;
+        String sql = "SELECT * FROM tasks where id = ? ";
         Connection con = DatabaseManager.getConnection();
-        Statement stmt = con.createStatement();
-        ResultSet result = stmt.executeQuery(selectOne);
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.clearParameters();
+        ps.setInt(1, id);
+        ResultSet result = ps.executeQuery();
         TasksBean tasks = new TasksBean();
         while (result.next()) {
             tasks.setId(result.getInt(1));
@@ -50,8 +52,52 @@ public class Tasks{
         return tasks;
     }
 
+    public TasksBean selectLikeTitle(String title) throws SQLException{
+        String sql = "SELECT * FROM tasks where title like ?";
+        Connection con = DatabaseManager.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.clearParameters();
+        ps.setString(1, "%" +  title + "%");
+        ResultSet result = ps.executeQuery();
+        TasksBean tasks = new TasksBean();
+        while (result.next()) {
+            tasks.setId(result.getInt(1));
+            tasks.setTitle(result.getString(2));
+            tasks.setFinished(result.getInt(3));
+            tasks.setVersion(result.getInt(4));
+            tasks.setCreatedAt(result.getTimestamp(5));
+            tasks.setUpdatedAt(result.getTimestamp(6));
+        }
+        con.close();
+        return tasks;
+    }
 
+    public int insert(TasksBean tasks) throws SQLException{
+        String sql = "INSERT INTO tasks(`title`, `finished`, `version`, `created_at`, `updated_at`) values(?, ?, ?, ?, ?)";
+        Connection con = DatabaseManager.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.clearParameters();
+        ps.setString(1,tasks.getTitle());
+        ps.setInt(2,tasks.getFinished());
+        ps.setInt(3,tasks.getVersion());
+        ps.setTimestamp(4,tasks.getCreatedAt());
+        ps.setTimestamp(5,tasks.getUpdatedAt());
+        System.out.print(ps);
+        int num = ps.executeUpdate();
+        System.out.print(num);
+        con.close();
+        return num;
+    }
 
-
+    public int deleteById(int id) throws SQLException{
+        String sql = "DELETE FROM tasks where id = ?";
+        Connection con = DatabaseManager.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.clearParameters();
+        ps.setInt(1, id);
+        int num = ps.executeUpdate();
+        con.close();
+        return num;
+    }
 
 }
--- src/test/java/app/sample/model/TasksTest.java
+++ src/test/java/app/sample/model/TasksTest.java
@@ -9,6 +9,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 
 import app.sample.bean.TasksBean;
@@ -55,8 +56,40 @@ public class TasksTest {
         Tasks task = new Tasks();
         TasksBean taskOne = task.selectById(5);
         assertEquals(taskOne.getId(),5);
-
     }
+
+    // テストメソッドに付与
+    @Test
+    public void selectByLikeTitleTest() throws SQLException {
+        Tasks task = new Tasks();
+        TasksBean taskOne = task.selectLikeTitle("title2");
+        assertEquals(taskOne.getId(),2);
+    }
+
+    // テストメソッドに付与
+/*
+    @Test
+    public void insertTest() throws SQLException{
+        Tasks task_instance = new Tasks();
+        TasksBean tasksbean = new TasksBean();
+        Timestamp now_time = new Timestamp(System.currentTimeMillis());
+        tasksbean.setTitle("insertTest");
+        tasksbean.setFinished(1);
+        tasksbean.setVersion(1234);
+        tasksbean.setCreatedAt(now_time);
+        tasksbean.setUpdatedAt(now_time);
+        task_instance.insert(tasksbean);
+        //assertEquals(task_instance.insert(tasksbean),2);
+    }
+*/
+    // テストメソッドに付与
+    //@Test
+    //public void deleteByIdTest() throws SQLException {
+    //    Tasks task = new Tasks();
+    //    int taskOne = task.deleteById(3);
+    //    assertEquals(taskOne,1);
+    //}
+
 
 
 
